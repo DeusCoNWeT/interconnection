62c3cbe540ac1b9c3819bc5603039826b34636d7
Miguel Ortega Moreno <mortega@conwet.com>
2018-03-19 12:56:26 +0100

    Add test to object properties

--- src/interconnection.js
+++ src/interconnection.js
@@ -1,3 +1,23 @@
+/*
+ * (C) Copyright 2018 Universidad Politécnica de Madrid.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Miguel Ortega Moreno
+ */
+
+
 (function (window, document) {
 
   // Init
@@ -282,7 +302,6 @@
       var fn = function (source, value, effect, old, fromAbove, dirtyCheck) {
         // translate the path notification to new path
         var notify_path = Polymer.Path.translate(source_prop, target_prop, source);
-        console.log(notify_path, source);
         // If dirty check is true, do it https://www.polymer-project.org/1.0/docs/devguide/model-data#override-dirty-check
         if (dirtyCheck) {
           target_el.set(target_prop, null);
--- test/specs/objectBinding.js
+++ test/specs/objectBinding.js
@@ -1,19 +1,132 @@
+/*
+ * (C) Copyright 2018 Universidad Politécnica de Madrid.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Miguel Ortega Moreno
+ */
 (function () {
-  describe('Stress test', function () {
-    var interconnection;
-    var targetElement;
+  'use strict';
+
+  describe('Bind object elements', function () {
+    var interconnection, el1, el2, el3;
     var assert = chai.assert;
     var expect = chai.expect;
-  
+
     before(function () {
       interconnection = window.Interconnection;
-      targetElement = document.querySelector('test-producer');
+      el1 = document.querySelector('#test-object');
+      el2 = document.querySelector('#test-object2');
+      el3 = document.querySelector('#test-object3');
     });
 
 
-    it('bind', function (done) {
-      var a =0;
+    it('bind two single object', function () {
+      interconnection.bind(el1, 'singleObject', el2, 'consumer');
+
+      assert.equal(el1.singleObject, el2.consumer, 'Both properties are different after binding');
+      el1.set('singleObject.x', 1000);
+      assert.exists(el2.consumer, ' El2 has not the same properties that el1. Consumer is missing');
+      assert.exists(el2.consumer.x, ' El2 has not the same properties that el1. consumer.x is missing');
+      assert.equal(el2.consumer.x, 1000, 'Property should be 1000 because el1 property has changed');
+
+      interconnection.unbindElement(el1);
     });
 
+    it('bind three elements in cascade', function () {
+      interconnection.bind(el1, 'singleObject', el2, 'consumer');
+      interconnection.bind(el2, 'consumer', el3, 'consumer');
+      assert.equal(el1.singleObject, el2.consumer, 'Both properties are different after binding. el1.singleObject -> el2.consumer');
+      assert.equal(el2.consumer, el3.consumer, 'Both properties are different after binding. el2.consumer -> el3.consumer');
+
+      el1.set('singleObject.x', 1000);
+
+      assert.exists(el2.consumer, ' El2 has not the same properties that el1. Consumer is missing');
+      assert.exists(el2.consumer.x, ' El2 has not the same properties that el1. consumer.x is missing');
+
+      assert.exists(el3.consumer, ' El3 has not the same properties that el2. Consumer is missing');
+      assert.exists(el3.consumer.x, ' El3 has not the same properties that el2. consumer.x is missing');
+
+      assert.equal(el2.consumer.x, 1000, 'Property should be 1000 because el1 property has changed');
+      assert.equal(el3.consumer.x, 1000, 'Property should be 1000 because el1 property has changed');
+
+      interconnection.unbindElement(el1);
+      interconnection.unbindElement(el2);
+    });
+
+    it('bind an object with a key that is an object', function () {
+      interconnection.bind(el1, 'deep', el2, 'consumer');
+      assert.equal(el1.deep, el2.consumer, 'Both properties are different after binding');
+
+      el1.set('deep.testx.x', 1000);
+      assert.exists(el2.consumer, ' El2 has not the same properties that el1. Consumer is missing');
+      assert.exists(el2.consumer.testx, ' El2 has not the same properties that el1. consumer.testx is missing');
+      assert.exists(el2.consumer.testx.x, ' El2 has not the same properties that el1. consumer.testx.x is missing');
+
+      assert.equal(el2.consumer.testx.x, 1000, ' El2 has not the same properties that el1. consumer.testx.x is missing');
+
+      el1.set('deep.testx', { x: 'change' });
+      assert.equal(el2.consumer.testx.x, 'change', ' El2 has not the same properties that el1. deep.testx changed but not in el2');
+
+      el1.set('deep', { testx: { x: 1 }, testy: { y: 1 } });
+      assert.equal(el2.consumer, el1.deep, ' El2 has not the same properties that el1. Deep changes are not notified');
+
+      interconnection.unbindElement(el1);
+    });
+
+    it('bind a deep property', function () {
+      interconnection.bind(el1, 'deep.testx', el2, 'consumer');
+      assert.equal(el1.deep.testx, el2.consumer, 'Both properties are different after binding');
+
+      el1.set('deep.testx', { x: 2 });
+
+      assert.equal(el2.consumer, el1.deep.testx, 'El2 has not the same properties that el1. el2.consumer has not change');
+
+      el1.set('deep.testx', { y: 2 });
+      assert.isUndefined(el2.consumer.x, 'Consumer doesnt change. consumer.x exist and el1 remove it');
+      assert.equal(el1.deep.testx, el2.consumer,'El2 has not the same properties that el1. el2.consumer has not change');
+
+      interconnection.unbindElement(el1);
+    });
+
+    it('multiple level ', function(){
+      interconnection.bind(el1,'deep',el2,'consumer');
+      interconnection.bind(el1,'deep.testx',el3,'consumer');
+
+      el1.set('deep.testy.y', 3);
+
+      assert.equal(el1.deep, el2.consumer,'El2 has not the same properties that el1. deep.testx changed but not in el2');
+      assert.equal(el2.testx, el3.consumer.testx, 'El3 has not the same properties that el1. deep.testx changed but not in el3');
+
+      el1.set('deep.testx.x', 3);
+      assert.equal(el1.deep, el2.consumer,'El2 has not the same properties that el1. deep.testx changed but not in el2');
+      assert.equal(el2.testx, el3.consumer.testx, 'El3 has not the same properties that el1. deep.testx changed but not in el3');
+      
+      interconnection.unbindElement(el1);
+      interconnection.unbindElement(el2);
+    });
+
+    it('test an object with duplicated keys value object', function(){
+
+      interconnection.bind(el1,'doubleKey', el2,'consumer');
+
+      assert.equal(el1.doubleKey, el2.consumer, 'el1 and el2 are not equals after binding');
+      
+      el1.set('doubleKey.doubleKey.x', 4);
+      assert.equal(el2.consumer,el1.doubleKey, 'El2 has not the same properties that el1. doubleKey.doubleKey.x changed but not in el2');
+
+      interconnection.unbindElement(el1);
+    });
   });
 })();
