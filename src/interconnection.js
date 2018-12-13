c604c8dffebac1e104dc2df7f01666a3cf34b1a9
Miguel Ortega Moreno <mortega@conwet.com>
2018-12-13 10:48:19 +0100

    fix findIndex

--- src/interconnection.js
+++ src/interconnection.js
@@ -269,7 +269,7 @@
         throw new Error('Source and target element must be a HTMLElement');
       }
 
-      if (!source_prop || !target_prop){
+      if (!source_prop || !target_prop) {
         throw new Error('Source or target property are empty');
       }
       if (source_el == target_el) {
@@ -474,7 +474,7 @@
         delete el_map.listeners[target_prop];
 
         var source_map = this.elementsMap.get(listener.source_el);
-        var idx = source_map.observers[listener.source_prop].findIndex( function(el){ el.fn === listener.fn});
+        var idx = source_map.observers[listener.source_prop].findIndex(function (el) { return el.fn === listener.fn });
 
         source_map.observers[listener.source_prop].splice(idx, 1);
 
